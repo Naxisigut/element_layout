04c4a3c0082a51b10f5d5894843dbec349241914
lcc <licc@bbieat.com>
2022-11-26 09:59:46 +0800

    bbi commit test

--- src/views/form/formItem-1.vue
+++ src/views/form/formItem-1.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <el-form ref="form" :model="form" label-width="80px">
-      <el-form-item label="活动名称">
+      <el-form-item label="活动名称11">
         <el-input v-model="form.name"></el-input>
       </el-form-item>
       <el-form-item label="活动区域">
