3eaadb1c8ca9ea5bcd0613078cf1f4b7e36000d8
lcc <licc@bbieat.com>
2022-12-09 15:34:20 +0800

    feat: 限制文件数量-列表可更新_2

--- src/views/uploader/uploader_use_4.vue
+++ src/views/uploader/uploader_use_4.vue
@@ -6,22 +6,24 @@
         action="https://jsonplaceholder.typicode.com/posts/"
         :auto-upload="false"
         :on-change="onChange"
+        :file-list="fileList"
       >
         <el-button size="small" type="primary">点击上传</el-button>
         <template #tip>
-          <div class="upload_tip" >文件最多三个</div>
+          <div class="upload_tip" >文件最多一个</div>
         </template>
       </el-upload>
-      <!-- <input type="file" @change="change"> -->
     </div>
     <div class="h100"></div>
     <div class="container desc">
       <h2>说明</h2>
       <div class="h10"></div>
       <ul class="descList">
-        <li> 文件数量超过3个后，可继续上传，之前的文件移除，保持数量为3个 </li>
+        <li> 文件数量超过1个后，可继续上传，之前的文件移除，保持数量为1个 </li>
+        <li> 绑定的fileList改变时，会把变化后的fileList重新赋给内部真正的fileList </li>
       </ul>
     </div>
+    
   </div>
 </template>
 
@@ -29,6 +31,7 @@
 export default {
   data() {
     return {
+      fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
     };
   },
   methods: {
@@ -36,9 +39,10 @@ export default {
      * 文件列表的添加/移除
      */
     onChange(file, fileList) {
-      console.log("fileList =", fileList)
-      if(fileList.length > 3) fileList = fileList.slice(-3)
-    },
+      if(fileList.length > 1) {
+        this.fileList = fileList.slice(-1) // 这里把包含真实fileList最后一个file的数组赋给this.fileList，更新fileList
+      }
+    }
   },
 };
 </script>
