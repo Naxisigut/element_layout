341fbb89a7a6a4aa4e4438bea88d106a483dba0f
Naxisigut <834525217@qq.com>
2022-12-21 19:02:12 +0800

    feat: 自定义分页组件

--- src/components/custom-pagination/index.vue
+++ src/components/custom-pagination/index.vue
@@ -1,14 +1,53 @@
 <template>
   <div class="custom-pagination">
-     custom pagination
+    <el-pagination
+      v-bind="pageConfig"
+      :current-page="current" 
+      :page-size="size" 
+      :total="total"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    ></el-pagination>
   </div>
 </template>
 
 <script>
 export default {
   name: 'custom-pagination',
+  props:{
+    current: {
+      type: Number,
+      default: 1
+    },
+    total: {
+      type: Number,
+      default: 1000,
+    },
+    size: {
+      type: Number,
+      default: 10
+    }
+  },
   data(){
-    return {}
+    return {
+      pageConfig: {
+        'layout':"prev, pager, next, sizes, jumper, total",
+        'page-sizes': [10, 20],
+      },
+      pageParams: {
+        current: 1,
+        total: 1000, 
+        size: 10
+      }
+    }
+  },
+  methods:{
+    handleSizeChange(val){
+      this.$emit('update:size', val) // sync
+    },
+    handleCurrentChange(val){
+      this.$emit('update:current', val) // sync
+    }
   }
 }
    
--- src/views/pagination/pagination_func_1.vue
+++ src/views/pagination/pagination_func_1.vue
@@ -1,23 +1,34 @@
 <template>
   <div class="page-container">
     <div class="container">
-      pagination
+      <custom-pagination 
+        :current.sync="pageParams.current"
+        :size.sync="pageParams.size"
+        :total="pageParams.total"
+      ></custom-pagination>
     </div>
     <div class="h100"></div>
     <div class="container desc">
       <h2>说明</h2>
       <div class="h10"></div>
       <ul class="descList">
-        <li>说明区</li>
+        <li>current和size在使用了.sync后，可以双向绑定数据：在组件上点击页码/每页数量后，绑定的数据会同步改变。</li>
       </ul>
     </div>
   </div>
 </template>
 
 <script>
+import customPagination from '@/components/custom-pagination';
 export default {
+  components:{ customPagination },
   data(){
     return {
+      pageParams: {
+        current: 1,
+        total: 1000, 
+        size: 10
+      }
     }
   }
 }
